e11f944cb503a0752eba1270f06f48c930dcaf01
Vitaliy Karakay <iipizpak@gmail.com>
2023-09-05 18:20:29 +0300

    [5] - added save logs to DB logic

--- go.mod
+++ go.mod
@@ -9,10 +9,13 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.9.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.15.2 // indirect
+	github.com/go-playground/validator/v10 v10.15.3 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/godror/godror v0.39.1 // indirect
+	github.com/godror/knownpb v0.1.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -24,6 +27,7 @@ require (
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.4.0 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
--- main/main.go
+++ main/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "time"
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	"github.com/godror/godror"
+	"net/http"
+	"time"
+)
 
 /*Необходимо создать сервис который будет принимать и сохранять логи от сервисов которые отправляют SMS и Email сообщения.
 Какие данные будут приходить:
@@ -23,7 +29,7 @@ messageID: '6774560000068401360004'
 Web: Gin */
 
 type Log struct {
-	UserId       int
+	UserID       int
 	Phone        string
 	ActionID     int
 	ActionTitle  string
@@ -39,5 +45,42 @@ type Log struct {
 }
 
 func main() {
+	r := gin.Default()
+	r.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{"Message": "Log Saver"})
+	})
+	r.POST("/log", logHandler)
 
+	r.Run(":8080")
+}
+func logHandler(c *gin.Context) {
+	var logData Log
+	if err := c.BindJSON(&logData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db, _ := SetupDB()
+
+	_, err := db.Exec("INSERT INTO log (user_id, phone, action_id, action_title, action_type, message, sender, status, language, full_response, created, updated, message_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
+		logData.UserID, logData.Phone, logData.ActionID, logData.ActionTitle, logData.ActionType,
+		logData.Message, logData.Sender, logData.Status, logData.Language, logData.FullResponse,
+		logData.Created, logData.Updated, logData.MessageId)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "database error"})
+		return
+	}
+	defer db.Close()
+
+	c.JSON(http.StatusOK, gin.H{"message": "log saved"})
+}
+
+func SetupDB() (*sql.DB, error) {
+	dsn := "hr/hr@//DESKTOP-DOVQPAO:1521/XEPDB1"
+	db, err := sql.Open(godror.DriverName, dsn)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
