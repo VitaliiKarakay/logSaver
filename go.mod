39ac4037ab316b9f989beb6d6902d2a045fdb423
Serhii Hatilov <serhii.hatilov@gmail.com>
2023-09-05 23:31:51 +0300

    play with Actions

--- go.mod
+++ go.mod
@@ -2,22 +2,18 @@ module logSaver
 
 go 1.21
 
-require (
-	github.com/gin-gonic/gin v1.9.1
-	github.com/godror/godror v0.39.2
-)
+require github.com/gin-gonic/gin v1.9.1
 
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/godror/knownpb v0.1.1 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -29,7 +25,6 @@ require (
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
--- main/main.go
+++ main/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/godror/godror"
 )
 
 /*Необходимо создать сервис который будет принимать и сохранять логи от сервисов которые отправляют SMS и Email сообщения.
@@ -50,38 +48,39 @@ func main() {
 	r.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"Message": "Log Saver"})
 	})
-	r.POST("/log", logHandler)
+	// r.POST("/log", logHandler)
 
 	r.Run(":8080")
 }
-func logHandler(c *gin.Context) {
-	var logData Log
-	if err := c.BindJSON(&logData); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
 
-	db, _ := SetupDB()
+// func logHandler(c *gin.Context) {
+// 	var logData Log
+// 	if err := c.BindJSON(&logData); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
 
-	_, err := db.Exec("INSERT INTO log (user_id, phone, action_id, action_title, action_type, message, sender, status, language, full_response, created, updated, message_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
-		logData.UserID, logData.Phone, logData.ActionID, logData.ActionTitle, logData.ActionType,
-		logData.Message, logData.Sender, logData.Status, logData.Language, logData.FullResponse,
-		logData.Created, logData.Updated, logData.MessageId)
+// 	db, _ := SetupDB()
 
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "database error"})
-		return
-	}
-	defer db.Close()
+// 	_, err := db.Exec("INSERT INTO log (user_id, phone, action_id, action_title, action_type, message, sender, status, language, full_response, created, updated, message_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
+// 		logData.UserID, logData.Phone, logData.ActionID, logData.ActionTitle, logData.ActionType,
+// 		logData.Message, logData.Sender, logData.Status, logData.Language, logData.FullResponse,
+// 		logData.Created, logData.Updated, logData.MessageId)
 
-	c.JSON(http.StatusOK, gin.H{"message": "log saved"})
-}
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "database error"})
+// 		return
+// 	}
+// 	defer db.Close()
 
-func SetupDB() (*sql.DB, error) {
-	dsn := "hr/hr@//DESKTOP-DOVQPAO:1521/XEPDB1"
-	db, err := sql.Open(godror.DriverName, dsn)
-	if err != nil {
-		return nil, err
-	}
-	return db, nil
-}
+// 	c.JSON(http.StatusOK, gin.H{"message": "log saved"})
+// }
+
+// func SetupDB() (*sql.DB, error) {
+// 	dsn := "hr/hr@//DESKTOP-DOVQPAO:1521/XEPDB1"
+// 	db, err := sql.Open(godror.DriverName, dsn)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return db, nil
+// }
