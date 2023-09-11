7c0af0c63d97f3e5619342f5656e8828f78a8de6
Vitaliy Karakay <iipizpak@gmail.com>
2023-09-11 10:39:52 +0300

    [17] - moved route logic to routes.go

--- cmd/logSaver/main.go
+++ cmd/logSaver/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
+	_ "github.com/sijms/go-ora/v2"
 	"log"
 	"logSaver/pkg/config"
 	http2 "logSaver/pkg/http"
 	"logSaver/pkg/store"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/sijms/go-ora/v2"
 )
 
 func main() {
@@ -22,17 +18,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	handler := http2.LogHandler{DB: db}
-	r := gin.Default() //должна запускаться в функции в http
-	r.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{"Message": "Log Saver"})
-	})
-	r.POST("/log", handler.HandleLog)
-
-	if err := r.Run(":8080"); err != nil {
-		fmt.Println(err)
-
-		return
+	err = http2.Run(db)
+	if err != nil {
+		log.Fatal(err)
 	} //конец
 
 	err = db.CloseConnection()
---
+++ pkg/http/routes.go
@@ -0,0 +1,25 @@
+package http
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"logSaver/pkg/store"
+	"net/http"
+)
+
+func Run(db *store.DB) error {
+	handler := LogHandler{DB: db}
+	r := gin.Default()
+	r.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{"Message": "Log Saver"})
+	})
+	r.POST("/log", handler.HandleLog)
+
+	if err := r.Run(":8080"); err != nil {
+		fmt.Println(err)
+
+		return err
+	}
+
+	return nil
+}
