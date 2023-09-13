e0336e391596c6e00be7d3835dddb3d21482896f
Vitaliy Karakay <iipizpak@gmail.com>
2023-09-13 21:53:54 +0300

    [tests] - added store suite test

--- pkg/store/store_suite_test.go
+++ pkg/store/store_suite_test.go
@@ -1,18 +1,60 @@
 package store_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 
+	"logSaver/pkg/config"
 	"logSaver/pkg/store"
 )
 
 type StoreSuite struct {
 	suite.Suite
 	Store *store.DB
+
+	tables []string
 }
 
-func RunSuite(t *testing.T) {
+func TestRunSuite(t *testing.T) {
 	suite.Run(t, new(StoreSuite))
+}
+
+func (s *StoreSuite) SetupSuite() {
+	s.tables = []string{
+		"logTest", //не забыть вынести в константу
+	}
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		fmt.Println("ReadConfig ", err)
+	}
+
+	db, err := store.New(cfg)
+	if err != nil {
+		fmt.Println("store.New ", err)
+	}
+
+	s.Store = db
+}
+
+func (s *StoreSuite) BeforeTest() {
+	s.cleanDB()
+}
+
+func (s *StoreSuite) TearDownTest() {
+	s.cleanDB()
+}
+
+func (s *StoreSuite) TearDownSuite() {
+	s.cleanDB()
+}
+
+func (s *StoreSuite) cleanDB() {
+	for _, table := range s.tables {
+		_, err := s.Store.Oracle.Exec(`TRUNCATE TABLE ` + table)
+		if err != nil {
+			fmt.Println("cleanDB ", err)
+		}
+	}
 }
